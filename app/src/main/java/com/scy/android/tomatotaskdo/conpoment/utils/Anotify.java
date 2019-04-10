cdc64c2e6818ba3cb967b155e916a0344fae4360
scy <1797484636@qq.com>
2019-04-10 16:29:59 +0800

    addTask notify

---
+++ app/src/main/java/com/scy/android/tomatotaskdo/conpoment/utils/Anotify.java
@@ -0,0 +1,10 @@
+package com.scy.android.tomatotaskdo.conpoment.utils;
+
+/**
+ * 沈程阳
+ * created by scy on 2019/4/10 16:28
+ * 邮箱：1797484636@qq.com
+ */
+interface Anotify {
+    void anotify();
+}
